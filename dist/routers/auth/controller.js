71cbfb78ecd558724185e76538e65103b95f6082
talhe <talhe9990@gmail.com>
2023-10-21 15:00:48 +0330

    second commit

--- dist/routers/auth/controller.js
+++ dist/routers/auth/controller.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -13,6 +36,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const user_1 = __importDefault(require("../../models/user"));
+const bcrypt = __importStar(require("bcrypt"));
+const jwt = __importStar(require("jsonwebtoken"));
 class AuthController {
     register(request, reply) {
         return __awaiter(this, void 0, void 0, function* () {
@@ -22,24 +47,48 @@ class AuthController {
                 where: { email: email },
             });
             if (user) {
-                reply.code(400).send({
+                return reply.code(400).send({
                     message: "The user already exists",
                 });
             }
+            // hash user password
+            const salt = yield bcrypt.genSalt(10);
+            const hash = yield bcrypt.hash(password, salt);
             // save user to db
-            const newuser = yield user_1.default.create({
+            const newUser = yield user_1.default.create({
                 name,
                 email,
-                password,
+                password: hash
             });
-            yield newuser.save();
+            yield newUser.save();
             reply.code(200).send({
                 message: "User registered successfully",
+                data: newUser
             });
         });
     }
     login(request, reply) {
         return __awaiter(this, void 0, void 0, function* () {
+            // check user
+            const user = yield user_1.default.findOne({ where: { email: request.body.email } });
+            if (!user) {
+                return reply.code(404).send({
+                    message: "User not found",
+                });
+            }
+            // check password
+            const isValid = yield bcrypt.compare(request.body.password, user.dataValues.password);
+            if (!isValid) {
+                return reply.code(400).send({
+                    message: "password or email is Invalid",
+                });
+            }
+            // create token
+            const accessToken = jwt.sign({ id: user.dataValues.id }, "jwt_key");
+            reply.code(200).send({
+                message: "successfuly logged in",
+                accessToken: accessToken
+            });
         });
     }
 }
--- src/routers/auth/controller.ts
+++ src/routers/auth/controller.ts
@@ -1,6 +1,7 @@
+
 import User from "../../models/user";
-
-
+import * as bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
 class AuthController {
 
   async register(request: any, reply: any) {
@@ -11,28 +12,49 @@ class AuthController {
       where: { email: email },
     });
     if (user) {
-      reply.code(400).send({
+     return reply.code(400).send({
         message: "The user already exists",
       }
       );
     }
-
+    // hash user password
+   const salt = await bcrypt.genSalt(10);
+   const hash =  await bcrypt.hash(password, salt);
     // save user to db
-    const newuser = await User.create({
+   const newUser = await User.create({
       name,
       email,
-      password,
+      password: hash
     });
 
-
-    await newuser.save();
+    await newUser.save();
     reply.code(200).send({
       message: "User registered successfully",
+      data: newUser
     })
   }
 
   async login(request: any, reply: any) {
-
+    // check user
+    const user = await User.findOne({where: {email: request.body.email}});
+    if(!user) {
+     return reply.code(404).send({
+        message: "User not found",
+      })
+    }
+    // check password
+    const isValid = await bcrypt.compare(request.body.password, user.dataValues.password);
+    if(!isValid){
+     return reply.code(400).send({
+      message: "password or email is Invalid",
+      })
+    }
+    // create token
+    const accessToken = jwt.sign({ id: user.dataValues.id }, "jwt_key");
+    reply.code(200).send({
+      message: "successfuly logged in",
+      accessToken: accessToken
+    })
   }
 }
 
