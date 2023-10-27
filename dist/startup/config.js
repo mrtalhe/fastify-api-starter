60f3a69a6e917b8ba1d2a3e3fca997c235efc9d5
talhe <talhe9990@gmail.com>
2023-10-27 18:19:12 +0330

    add prisma util

--- dist/startup/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(app, express) {
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.static('public'));
-}
-exports.default = default_1;
